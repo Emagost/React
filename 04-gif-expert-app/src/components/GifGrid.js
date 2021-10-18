a29634e041735a662e5b66d30484b57ad024688d
Emanuel <emagozz@gmail.com>
2021-10-18 00:00:56 -0300

    Add more components and render gifs-texts

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
     
-    const getGifs = async () => {
+    const [images, setImages] = useState([]);
 
+    useEffect(() =>{
+        getGifs();
+    }, [])
+
+    const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=The+office&limit=10&api_key=uMmbJMSSc1X2KPsdKbAdWzDy8o0U0juX';
         const resp = await fetch(url);
         const {data} = await resp.json();
@@ -13,19 +19,31 @@ export const GifGrid = ( {category} ) => {
                 id: img.id,
                 title:img.title,
                 url: img.images?.downsized_medium.url,
-            }
-        })
-
+            };
+        });
         console.log(gifs);
+        setImages( gifs );
 
 
-    }
+
+
+    };
     
-    getGifs();
-    
+
+
+
+
     return (
         <div>
             <h3>{ category }</h3>
+            {
+                images.map( img => (
+                    <GifGridItem 
+                    key = { img.id }
+                    { ...img }
+                    />
+                ))
+            };
         </div>
-    )
-}
+    );
+};
