02508953261fc49e87cd3d14c91ff24fec542ed0
Emanuel Gomez <emagozz@gmail.com>
2021-10-25 19:47:31 -0300

    start test about the app

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const GifGridItem = ( {id,title,url} ) => {
+const GifGridItem = ( {id,title,url} ) => {
     return (
         <div className="card animate__animated animate__fadeIn">
                 <img src={ url } alt={ title }/>
@@ -8,3 +8,5 @@ export const GifGridItem = ( {id,title,url} ) => {
         </div>
     )
 }
+
+export default GifGridItem;
