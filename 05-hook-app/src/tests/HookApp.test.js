3e6968bfe5e544c0570733550ab1218d1575b7a7
Emanuel Gomez <emagozz@gmail.com>
2021-11-04 18:23:42 -0300

    init test for hooks using enzyme/jest

---
+++ 05-hook-app/src/tests/HookApp.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import { HookApp } from "../HookApp";
+
+
+describe('Test hookApp', () => {
+    test('snapshot', () => {
+        
+        const wrapper = shallow(<HookApp/>);
+
+        expect(wrapper).toMatchSnapshot();
+
+    })
+    
+})
