058c93af315993e46ed75b5442ef953e0f6c5daf
Emanuel Gomez <emagozz@gmail.com>
2021-11-15 20:37:58 -0300

    HeroesApp, add a lot of css

--- 07-heroes-app/src/components/heroes/HeroesList.js
+++ 07-heroes-app/src/components/heroes/HeroesList.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import getHeroesByPublisher from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroesList = ({publisher}) => {
 
     const heroes= getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard key={hero.id}
+                    {...hero }
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }
