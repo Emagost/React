06487ef1d025c2a4c67eda36299912ca083b756e
Emanuel Gomez <emagozz@gmail.com>
2021-11-15 22:46:45 -0300

    fix more bugs

--- 07-heroes-app/src/components/heroes/HeroCard.js
+++ 07-heroes-app/src/components/heroes/HeroCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HeroCard.css';
+
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
     return (
-        <Link to={`./hero/${id}`} className="my-card">
+        <Link to={`./hero/${ id }`} className="my-card">
             <img src={`./assets/heroes/${id}.jpg`} className="img img-responsive" alt={superhero}/>
             <div className="profile-name">{superhero}</div>
             <div className="profile-position">{alter_ego}</div>
