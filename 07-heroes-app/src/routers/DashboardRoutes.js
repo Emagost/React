cb9d8805ef241c73115a349539d744bed94c21d3
Emanuel Gomez <emagozz@gmail.com>
2021-11-16 17:21:22 -0300

    fix bugs and add item search

--- 07-heroes-app/src/routers/DashboardRoutes.js
+++ 07-heroes-app/src/routers/DashboardRoutes.js
@@ -1,8 +1,10 @@
-import React from 'react';
 import { Navbar } from '../components/ui/Narbar';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { HeroScreen } from '../components/heroes/HeroScreen';
 import { DcScreen } from '../components/dc/DcScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
+
+import React from 'react';
 import {Route} from "react-router-dom";
 import { Navigate, Routes } from '../../node_modules/react-router-dom/index';
 
@@ -12,16 +14,17 @@ export const DashboardRoutes = () => {
             <Navbar/>
 
             <div className="container mt-2">
-            <Routes>
-                <Route path="/marvel" element={<MarvelScreen />} />
-                <Route path="marvel/hero/:heroeId" element={<HeroScreen />} />
-                <Route path="dc/hero/:heroeId" element={<HeroScreen />} />
-                <Route path="/dc" element={<DcScreen />} />
-                <Route path="/*" element={ <Navigate replace to="/marvel" /> } />
+                <Routes>
+                    <Route path="/marvel" element={<MarvelScreen />} />
+                    <Route path="marvel/hero/:heroeId" element={<HeroScreen />} />
+                    <Route path="dc/hero/:heroeId" element={<HeroScreen />} />
+                    <Route path="/dc" element={<DcScreen />} />
+                    <Route path="/search" element={<SearchScreen />} />
 
-            </Routes>
-                
+                    
+                    <Route path="/*" element={ <Navigate replace to="/marvel" /> } />
 
+                </Routes>
             </div>
 
         </>
